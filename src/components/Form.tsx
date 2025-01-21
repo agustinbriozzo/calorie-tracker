b2ec6c8fd3e884a7a6e40100dd1841ae6955c9eb
Agustin Briozzo <agustinbriozzo1997@gmail.com>
2025-01-20 21:11:57 -0300

    Create type for activities, set values in the state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
+import { Activity } from "../types";
 import { categories } from "../data/categories";
 export default function Form() {
-  const [activity, setActivity] = useState({
-    category: 2,
+  const [activity, setActivity] = useState<Activity>({
+    category: 1,
     name: "",
     calories: 0,
   });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
+    });
+  };
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
@@ -16,6 +27,7 @@ export default function Form() {
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
           value={activity.category}
+          onChange={handleChange}
         >
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -35,6 +47,7 @@ export default function Form() {
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
           value={activity.name}
+          onChange={handleChange}
         />
       </div>
 
@@ -48,6 +61,7 @@ export default function Form() {
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Calorias"
           value={activity.calories}
+          onChange={handleChange}
         />
       </div>
 
