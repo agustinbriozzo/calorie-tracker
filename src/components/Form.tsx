6c406c94e926e691d2f57b8b1a7efe1f6929bd03
Agustin Briozzo <agustinbriozzo1997@gmail.com>
2025-01-30 22:03:42 -0300

    Define reducer

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,12 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
 import { Activity } from "../types";
 import { categories } from "../data/categories";
-export default function Form() {
+import { ActivityActions } from "../reducers/activity-reducer";
+
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>;
+};
+export default function Form({ dispatch }: FormProps) {
   const [activity, setActivity] = useState<Activity>({
     category: 1,
     name: "",
@@ -22,8 +27,16 @@ export default function Form() {
     const { name, calories } = activity;
     return name.trim() !== "" && calories > 0;
   };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "save-activity", payload: { newActivity: activity } });
+  };
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
+    >
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">
           Categoría:
