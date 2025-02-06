61c7cc99514101b55f4075bd6948ba4bc3b5b8cb
Agustin Briozzo <agustinbriozzo1997@gmail.com>
2025-02-06 17:05:59 -0300

    Show calories and difference

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer, useEffect, useMemo } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
+import CalorieTracker from "./components/CalorieTracker";
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
   useEffect(() => {
@@ -33,6 +34,12 @@ function App() {
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} state={state} />
+        </div>
+      </section>
+
+      <section className="bg-gray-900 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker activities={state.activities} />
         </div>
       </section>
 
